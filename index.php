8df94bb544cc31e285ab6d4e9e342d75f912a2e9
Thomas Weily <twweily@gmail.com>
2021-08-01 16:19:12 +1200

    HTML to PHP
    Replaced index.html file extension to index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <!-- 
+            Welcome to my websites source code.
+
+            This page was built with the help of Bootstrap.
+         -->
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
